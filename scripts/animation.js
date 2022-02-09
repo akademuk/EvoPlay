dd9b5d7f88f9e55ee067c50d62c20d41e61a5373
Oleh Fedorov <akademuk24@gmail.com>
2022-02-09 04:24:23 +0200

    first commit

---
+++ scripts/animation.js
@@ -0,0 +1,17 @@
+const time = 1000;
+const step = 1;
+
+function outNum(num, elem) {
+  let e = document.querySelector("#out");
+  n = 0;
+  let t = Math.round(time / (num / step));
+  let interval = setInterval(() => {
+    n = n + step;
+    if (n == num) {
+      clearInterval(interval);
+    }
+    e.innerHTML = n;
+  }, t);
+}
+
+outNum(100, "#out");
